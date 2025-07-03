2b39a456722b1b41fc10145f004167cc347a1395
huseyindol-hg <huseyin.dol@hangikredi.com>
2025-07-04 02:34:17 +0300

    Revert "Add dotenv support for environment variable management, update .env file with new configurations, and enhance HTTP server and infrastructure files to load environment variables. Update package dependencies for dotenv and its types."
    This reverts commit 3b75335a28afe0838a2427949e7a3b85313217b8.

--- src/infrastructure/config/container.ts
+++ src/infrastructure/config/container.ts
@@ -1,6 +1,3 @@
-// Load environment variables from .env file
-import 'dotenv/config';
-
 import { LoanRepository, BankRepository } from '../../domain/repositories/index.js';
 import { LoanCalculationService } from '../../domain/services/index.js';
 import { AIService } from '../../application/interfaces/index.js';
